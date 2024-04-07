5c3f4dd0b26897e22afbd23999fac587c1ca3947
bitstrips18 <160264841+bitstrips18@users.noreply.github.com>
2024-04-06 19:48:11 -0700

    Update BirdAnim.java

--- src/zoosim/BirdAnim.java
+++ src/zoosim/BirdAnim.java
@@ -11,6 +11,10 @@ package zoosim;
 public class BirdAnim extends Animal implements IFlyable{
     public boolean canFly;
     public boolean flying;
+    BirdAnim(String sound,Gender sex, int size, String name, int age) {
+		super(sound,sex,size,name,age);
+		
+	}
     //The location of the animal, either landed or flying
     /**
      * If the animal is currently grounded, makes it fly
